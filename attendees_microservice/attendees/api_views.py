b4eef682852eb882cb99a859fe67bc115ee0a1b9
Rey <reyxeka@gmail.com>
2023-05-15 20:46:15 -0400

    Did stuff maybe works

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from .models import Attendee
-from events.models import Conference
-from events.api_views import ConferenceListEncoder
+from .models import Attendee, ConferenceVO
 from django.views.decorators.http import require_http_methods
 import json
+
+
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
 
 
 class AttendeeListEncoder(ModelEncoder):
@@ -24,14 +27,14 @@ class AttendeeDetailEncoder(ModelEncoder):
         "conference",
     ]
     encoders = {
-        "conference": ConferenceListEncoder(),
+        "conference": ConferenceVODetailEncoder(),
     }
 
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id):
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
@@ -39,9 +42,10 @@ def api_list_attendees(request, conference_id):
     else:
         content = json.loads(request.body)
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f'/api/conferences/{conference_vo_id}/'
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
@@ -71,9 +75,9 @@ def api_show_attendee(request, id):
         content = json.loads(request.body)
         try:
             if "conference" in content:
-                conference = Conference.objects.get(id=content['conference'])
+                conference = ConferenceVO.objects.get(id=content['conference'])
                 content['conference'] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
